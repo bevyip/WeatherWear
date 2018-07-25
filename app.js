7d41a82d72d3257c0997054f21eb12aa7d86439d
hyip8000 <hyip@purdue.edu>
2018-07-25 16:30:40 -0700

    removed all unnecessary UserInput stuff

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var dateLogRouter = require('./routes/dateLog');
-var userInputRouter = require('./routes/userInput');
 
 var app = express();
 
@@ -28,7 +27,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/dateLog', dateLogRouter);
-app.use('/userInput', userInputRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/dateLog.js
+++ routes/dateLog.js
@@ -7,11 +7,6 @@ const DateLog = require('../models/dateLog');
 const date_log = require('./dateLog')
 const User = require('../models/user');
 // const Weather = require('../models/weather');
-//
-// // goes to show all userInputs page (dateLog/ show.hbs)
-// router.get('/', auth.requireLogin, (req, res, next) => {
-//   DateLog.find()
-// });
 
 // when a form is submitted, it saves the new input and redirects
 // to dateLog/ show.hbs - all dateLogs
@@ -29,15 +24,11 @@ router.post('/', auth.requireLogin, (req, res, next) => {
 // displays dateLog/ show.hbs - all dateLogs
 router.get('/all', auth.requireLogin, (req, res, next) => {
   const currentUserId = req.session.userId;
-  // filter!!!!
-  // const resultArr = dateLogArr.filter(dateLog =>  dateLog.date == "the date I need by ID")
-  // DateLog.find({users: res.locals.currentUserId, date: req.locals.dateLogId.date}, function(err, dateLog){
-  console.log(currentUserId);
+
   DateLog.find({user: currentUserId}, function(err, dateLogs){
     if(err) {
       console.error(err);
     } else {
-      console.log(dateLogs);
       res.render('dateLog/all', { currentUserId: currentUserId, dateLogs: dateLogs });
     }
   });
@@ -57,6 +48,4 @@ router.get('/new', auth.requireLogin, (req, res, next) => {
   res.render('dateLog/new', { currentUserId: currentUserId });
 });
 
-
-// router.use('/userInput', user_input);
 module.exports = router;
